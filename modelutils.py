cbe0a5ee4426cad1af515ac433919c76802a8b2a
Denis Kuznedelev <dkuznedelev@qavm-efb872e7.qemu>
2023-06-11 21:38:14 +0300

    Refactor and README edit

--- modelutils.py
+++ modelutils.py
@@ -15,8 +15,6 @@ def get_model(model_path, dtype="auto"):
         low_cpu_mem_usage=True,  # see https://stackoverflow.com/questions/76356591
     )
     model.seqlen = 2048
-    if model.config.model_type == "RefinedWebModel":
-        fix_rotary_embedding(model)
     return model
 
 
@@ -47,70 +45,6 @@ def get_lm_logits(inps_, model):
     else:
         raise ValueError(MODEL_ERROR_MSG.format(model.config.model_type))
     return lm_logits
-
-
-# rotary pos emb helpers (torch.jit.script does not seem to support staticmethod...)
-def rotate_half(x):
-    x1, x2 = x[..., : x.shape[-1] // 2], x[..., x.shape[-1] // 2 :]
-    return torch.cat(
-        (-x2, x1), dim=x1.ndim - 1
-    )  # dim=-1 triggers a bug in torch < 1.8.0
-
-
-class RotaryEmbedding(torch.nn.Module):
-    """Implementation of RotaryEmbedding from GPT-NeoX.
-    This implementation is design to operate on queries and keys that are compatible with
-    [batch_size, n_heads_per_partition, seq_len, head_dim] (e.g. MinGPTAttention format).
-    """
-
-    def __init__(
-        self,
-        head_dim: int,
-        base=10000,
-    ):
-        super().__init__()
-        inv_freq = 1.0 / (base ** (torch.arange(0, head_dim, 2).float() / head_dim))
-        self.register_buffer("inv_freq", inv_freq, persistent=False)
-        self.head_dim = head_dim
-        self.seq_len_cached = None
-        self.batch_size_cached = None
-        self.cos_cached = None
-        self.sin_cached = None
-
-    def cos_sin(
-        self,
-        seq_len: int,
-        device="cuda",
-        dtype=torch.bfloat16,
-    ) -> torch.Tensor:
-        if seq_len != self.seq_len_cached:
-            self.seq_len_cached = seq_len
-            t = torch.arange(seq_len, device=device).type_as(self.inv_freq)
-            freqs = torch.einsum("i,j->ij", t, self.inv_freq)
-            emb = torch.cat((freqs, freqs), dim=-1).to(device)
-
-            if dtype != emb.dtype:
-                emb = emb.to(dtype)
-
-            self.cos_cached = emb.cos()[None, :, :]
-            self.sin_cached = emb.sin()[None, :, :]
-
-            self.cos_cached = self.cos_cached.type(dtype)
-            self.sin_cached = self.sin_cached.type(dtype)
-
-        return self.cos_cached, self.sin_cached
-
-    def forward(self, q, k):
-        batch, seq_len, head_dim = q.shape
-        cos, sin = self.cos_sin(seq_len, q.device, q.dtype)
-        return (q * cos) + (rotate_half(q) * sin), (k * cos) + (rotate_half(k) * sin)
-
-
-def fix_rotary_embedding(model):
-    for module in model.modules():
-        if hasattr(module, "maybe_rotary"):  # use for switch
-            head_dim = module.head_dim
-            module.maybe_rotary = RotaryEmbedding(head_dim)
 
 
 def get_layers(model):
