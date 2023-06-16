1f12a8690ba988334c4e88795c3a73b3966bfb2b
Ruslan Svirshevskiy <ruslansv@gmail.com>
2023-06-17 00:06:53 +0300

    rolled back some edits in lm_eval

--- lmeval.py
+++ lmeval.py
@@ -1,19 +1,9 @@
 import argparse
 import json
 import logging
+import fnmatch
 
-import os
-import sys
-
-from main import quantize_model
-from datautils import get_loaders
-from spqr_config import QuantizationConfig
-
-import_path = os.environ.get("LM_EVAL_HARNESS_PATH", "./lm-evaluation-harness")
-print(f"{import_path=}")
-sys.path.append(import_path)
-import lm_eval.models
-from lm_eval import tasks, evaluator, utils
+from lm_eval import tasks, evaluator
 
 try:
     import wandb
@@ -25,14 +15,29 @@ except:
 logging.getLogger("openai").setLevel(logging.WARNING)
 
 
+class MultiChoice:
+    def __init__(self, choices):
+        self.choices = choices
+
+    # Simple wildcard support (linux filename patterns)
+    def __contains__(self, values):
+        for value in values.split(","):
+            if len(fnmatch.filter(self.choices, value)) == 0:
+                return False
+
+        return True
+
+    def __iter__(self):
+        for choice in self.choices:
+            yield choice
+
+
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--model", required=True)
     parser.add_argument("--model_args", default="")
     parser.add_argument("--quantization_args", default=None)
-    parser.add_argument(
-        "--tasks", default=None, choices=utils.MultiChoice(tasks.ALL_TASKS)
-    )
+    parser.add_argument("--tasks", default=None, choices=MultiChoice(tasks.ALL_TASKS))
     parser.add_argument("--provide_description", action="store_true")
     parser.add_argument("--num_fewshot", type=int, default=0)
     parser.add_argument("--batch_size", type=int, default=None)
@@ -48,6 +53,16 @@ def parse_args():
     return parser.parse_args()
 
 
+# Returns a list containing all values of the source_list that
+# match at least one of the patterns
+def pattern_match(patterns, source_list):
+    task_names = set()
+    for pattern in patterns:
+        for matching in fnmatch.filter(source_list, pattern):
+            task_names.add(matching)
+    return list(task_names)
+
+
 def main():
     args = parse_args()
 
@@ -57,14 +72,12 @@ def main():
         wandb.init(config=args)
 
     if args.limit:
-        print(
-            "WARNING: --limit SHOULD ONLY BE USED FOR TESTING. REAL METRICS SHOULD NOT BE COMPUTED USING LIMIT."
-        )
+        print("WARNING: --limit SHOULD ONLY BE USED FOR TESTING. REAL METRICS SHOULD NOT BE COMPUTED USING LIMIT.")
 
     if args.tasks is None:
         task_names = tasks.ALL_TASKS
     else:
-        task_names = utils.pattern_match(args.tasks.split(","), tasks.ALL_TASKS)
+        task_names = pattern_match(args.tasks.split(","), tasks.ALL_TASKS)
 
     print(f"Selected Tasks: {task_names}")
 
