4ae4f6652c408432d3eded42ea0f948b2e207b37
blackadder <scope.denis@mail.ru>
2023-11-14 08:18:52 +0100

    Added MetaMath

---
+++ MetaMath/eval_gsm8k.py
@@ -0,0 +1,328 @@
+import re
+import torch
+import argparse
+import jsonlines
+import accelerate
+from tqdm import tqdm
+from fraction import Fraction
+
+from transformers import AutoTokenizer, GenerationConfig, StoppingCriteria, StoppingCriteriaList
+# local imports
+import sys
+sys.path.append('..')
+from main import get_model, quantize_model
+
+MAX_INT = sys.maxsize
+
+
+class StopStringCriterion(StoppingCriteria):
+    
+    def __init__(self, tokenizer, stops = []):
+        # tokenize stops
+        self.stops = stops
+        self.tokenizer = tokenizer
+
+    def __call__(self, input_ids: torch.LongTensor, scores: torch.FloatTensor, **kwargs) -> bool:
+        for seq in input_ids:
+            for stop in self.stops:
+                decoded_seq = self.tokenizer.decode(seq.cpu())
+                if decoded_seq.endswith(stop):
+                    return True
+        return False
+
+
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        pass
+    try:
+        import unicodedata
+        unicodedata.numeric(s)
+        return True
+    except (TypeError, ValueError):
+        pass
+    return False
+
+def extract_answer_number(completion):
+    text = completion.split('The answer is: ')
+    if len(text) > 1:
+        extract_ans = text[-1].strip()
+        match = re.search(r'[\-+]?\d*[\.,/]?\d+', extract_ans)
+        if match:
+            if '/' in match.group():
+                denominator = match.group().split('/')[1]
+                numerator = match.group().split('/')[0]
+                if is_number(denominator) == True and is_number(numerator) == True:
+                    if denominator == '0':
+                        return round(float(numerator.replace(',', '')))
+                    else:
+                        frac = Fraction(match.group().replace(',', ''))
+                        num_numerator = frac.numerator
+                        num_denominator = frac.denominator
+                        return round(float(num_numerator / num_denominator))
+                else:
+                    return None
+            else:
+                if float(match.group().replace(',', '')) == float('inf'):
+                    return None
+                return round(float(match.group().replace(',', '')))
+        else:
+            return None
+    else:
+        return None
+
+def batch_data(data_list, batch_size=1):
+    n = len(data_list) // batch_size
+    batch_data = []
+    for i in range(n-1):
+        start = i * batch_size
+        end = (i+1)*batch_size
+        batch_data.append(data_list[start:end])
+
+    last_start = (n-1) * batch_size
+    last_end = MAX_INT
+    batch_data.append(data_list[last_start:last_end])
+    return batch_data
+
+def auto_map_model(model, args):
+    if args.dtype == 'auto':
+        args.dtype = model.config.torch_dtype
+    device_map = accelerate.infer_auto_device_map(model, dtype=args.dtype)
+    for module_name, module in model.named_modules():
+        device = device_map.get(module_name)
+        if device is not None:
+            module = module.to(device)
+    return model
+
+def gsm8k_test(
+    model, 
+    args, 
+    data_path, 
+    start=0, 
+    end=MAX_INT, 
+    batch_size=1,
+):
+    # get device
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    INVALID_ANS = "[invalid]"
+    gsm8k_ins = []
+    gsm8k_answers = []
+    problem_prompt = (
+        "Below is an instruction that describes a task. "
+        "Write a response that appropriately completes the request.\n\n"
+        "### Instruction:\n{instruction}\n\n### Response: Let's think step by step."
+    )
+    print('promt =====', problem_prompt)
+    with open(data_path,"r+", encoding="utf8") as f:
+        for idx, item in enumerate(jsonlines.Reader(f)):
+            temp_instr = problem_prompt.format(instruction=item["query"])
+            gsm8k_ins.append(temp_instr)
+            temp_ans = item['response'].split('#### ')[1]
+            temp_ans = int(temp_ans.replace(',', ''))
+            gsm8k_answers.append(temp_ans)
+
+    gsm8k_ins = gsm8k_ins[start:end]
+    gsm8k_answers = gsm8k_answers[start:end]
+    print('lenght ====', len(gsm8k_ins))
+    batch_gsm8k_ins = batch_data(gsm8k_ins, batch_size=batch_size)
+
+    # get tokenizer
+    tokenizer = AutoTokenizer.from_pretrained(args.model)
+    # set [PAD] to [EOS] in case of absence if pad_token is not defined
+    tokenizer.pad_token = getattr(tokenizer, 'pad_token', tokenizer.eos_token)
+    # list of stop tokens
+    stop_tokens = ["Question:", "Question", "USER:", "USER", "ASSISTANT:", "ASSISTANT", "Instruction:", "Instruction", "Response:", "Response"]
+    generation_config = GenerationConfig(temperature=0.0, top_p=1, max_new_tokens=512)
+    stopping_criterion = StopStringCriterion(tokenizer, stop_tokens)
+    print('Sampling =====', generation_config)
+    # init model
+    llm = get_model(args.model, dtype=args.dtype)
+    # update args
+    args.dataset = args.calibration_dataset
+    args.model_path = args.model
+    _, wbits_avg = quantize_model(llm, args, device)
+    print(f'Average number of bits {wbits_avg:.2f}')
+    # distribute model among workers
+    if args.auto_device_map:
+        llm = auto_map_model(model, args)
+    else:
+        llm = llm.to(device)
+
+    result = []
+    res_completions = []
+    for idx, (prompt, prompt_answer) in tqdm(
+        enumerate(zip(batch_gsm8k_ins, gsm8k_answers)), 
+        total=len(batch_gsm8k_ins),
+        desc='Generating completions'
+    ):
+        if isinstance(prompt, list):
+            pass
+        else:
+            prompt = [prompt]
+
+        inputs = tokenizer(prompt, padding=True, return_tensors='pt', add_special_tokens=False)['input_ids']
+
+        completions = llm.generate(
+            inputs.to(device), 
+            generation_config=generation_config, 
+            stopping_criteria=StoppingCriteriaList([stopping_criterion])
+        )
+
+        for output in completions:
+            generated_text = tokenizer.decode(output.cpu(), skip_special_tokens=True)
+            res_completions.append(generated_text)
+
+    invalid_outputs = []
+    for idx, (prompt, completion, prompt_answer) in enumerate(zip(gsm8k_ins, res_completions, gsm8k_answers)):
+        doc = {'question': prompt}
+        y_pred = extract_answer_number(completion)
+        if y_pred != None:
+            result.append(float(y_pred) == float(prompt_answer))
+        else:
+            result.append(False)
+            temp = {'question': prompt, 'output': completion, 'answer': prompt_answer}
+            invalid_outputs.append(temp)
+    acc = sum(result) / len(result)
+    print('len invalid outputs ====', len(invalid_outputs), ', valid_outputs===', invalid_outputs)
+    print('start===', start, ', end====', end)
+    print('gsm8k length====', len(result), ', gsm8k acc====', acc)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model", type=str)  # model path
+    parser.add_argument("--data_file", type=str, default='')  # data path
+    parser.add_argument("--start", type=int, default=0) #start index
+    parser.add_argument("--end", type=int, default=MAX_INT)  # end index
+    parser.add_argument("--batch_size", type=int, default=400)  # batch_size
+    parser.add_argument("--auto_device_map", action='store_true', help='Whether to place parts of model onto devices')
+    # Quantization params
+    parser.add_argument(
+        "--calibration_dataset",
+        type=str,
+        default="none",
+        help="Dataset name [c4, pajama, refinedweb, none, etc.] or path to data where to extract calibration data from.",
+    )
+    parser.add_argument(
+        "--custom_data_path",
+        type=str,
+        default=None,
+        help="Path to load if specified. Deprecated",
+    )
+    parser.add_argument("--seed", type=int, default=0, help="Seed for sampling the calibration data.")
+    parser.add_argument("--nsamples", type=int, default=128, help="Number of calibration data samples.")
+    parser.add_argument(
+        "--percdamp",
+        type=float,
+        default=0.01,
+        help="Percent of the average Hessian diagonal to use for dampening.",
+    )
+    parser.add_argument("--nearest", action="store_true", help="Whether to run the RTN baseline.")
+    parser.add_argument(
+        "--wbits",
+        type=int,
+        default=16,
+        choices=[2, 3, 4, 8, 16],
+        help="#bits to use for quantization; use 16 for evaluating base model.",
+    )
+    parser.add_argument(
+        "--groupsize",
+        type=int,
+        default=None,
+        help="How many weight columns (input features) are quantized with the same statistics, default = all of them",
+    )
+    parser.add_argument(
+        "--permutation_order",
+        type=str,
+        default="identity",
+        help="Weights permutation order; options: identity(default), spearman, act_order",
+    )
+    parser.add_argument(
+        "--true-sequential",
+        action="store_true",
+        help="Whether to run in true sequential model.",
+    )
+    parser.add_argument(
+        "--new_eval",
+        action="store_true",
+        help="if this is set, evaluate on new (and slightly more realistic!) val dataset versions",
+    )
+    parser.add_argument("--sym", action="store_true", help="Symmetric quantization")
+    parser.add_argument(
+        "--perchannel",
+        action="store_true",
+        help="fit a unique quantizer to each output dim",
+    )
+    parser.add_argument(
+        "--qq_scale_bits",
+        type=int,
+        default=None,
+        help="Quantize quantization scale with this many bits (default=do not quantize)",
+    )
+    parser.add_argument(
+        "--round_zero",
+        type=int,
+        default=None,
+        help='whether to allow non-integer "zero" when quantizing weights non-symmetrically',
+    )
+    parser.add_argument(
+        "--qq_zero_bits",
+        type=int,
+        default=None,
+        help='Quantize quantization "zero" with this many bits (default=do not quantize)',
+    )
+    parser.add_argument(
+        "--qq_zero_sym",
+        action="store_true",
+        help="enable sym=True in meta-quantization for groupwise zero, specifically",
+    )
+    parser.add_argument(
+        "--qq_groupsize",
+        type=int,
+        default=16,
+        help="Quantize quantization scale in groups of this many scales",
+    )
+    parser.add_argument(
+        "--outlier_threshold",
+        type=float,
+        default=float("inf"),
+        help="relative threshold for     outliers; higher threshold = more outliers.",
+    )
+    parser.add_argument(
+        "--simplified_outliers",
+        action="store_true",
+        help="do not perform leave-one-out evaluation when detecting outliers; works faster, but generally worse in perplexity",
+    )
+    parser.add_argument("--wandb", action="store_true", help="Whether to use wandb or store locally.")
+    parser.add_argument(
+        "--skip_out_loss",
+        action="store_true",
+        help="Whether to skip computation of out loss.",
+    )
+    parser.add_argument(
+        "--offload_activations",
+        action="store_true",
+        help="Offload activations to RAM to save GPU memory.",
+    )
+    parser.add_argument(
+        "--dtype",
+        type=str,
+        default="auto",
+        choices=["auto", "float16", "float32"],
+        help="dtype to load the model.",
+    )
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    args = parse_args()
+    gsm8k_test(
+        model=args.model, 
+        args=args,
+        data_path=args.data_file, 
+        start=args.start, 
+        end=args.end, 
+        batch_size=args.batch_size, 
+    )
