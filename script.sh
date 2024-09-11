dc5ad187fab94dcdf9eb663e9ed2cb41b35eea3e
Elvir Crncevic <elvircrn@gmail.com>
2024-09-11 14:49:48 +0200

    Remove absolute path

--- script.sh
+++ script.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# export MODEL_PATH=/nfs/scistore19/alistgrp/ecrncevi/SpQR/llama/llama-2-7b/consolidated.00.pth
-export MODEL_PATH=/media/elvircrn/6e3c126c-c6bb-43eb-9d82-1e59b2111688/ecrncevi/SpQR/Llama-2-7b-hf
-export DATASET=/media/elvircrn/6e3c126c-c6bb-43eb-9d82-1e59b2111688/ecrncevi/SpQR/AQLM/data/red_pajama_n=4096_4096_context_length_llama.pth
-# export DATASET=/nfs/scistore19/alistgrp/ecrncevi/SpQR/data/red_pajama_n=1024.pth
+export MODEL_PATH=
+export DATASET=
 
 python3 main.py $MODEL_PATH $DATASET \
     --wbits 3 \
@@ -16,4 +14,4 @@ python3 main.py $MODEL_PATH $DATASET \
     --permutation_order act_order \
     --percdamp 1e0 \
     --nsamples 128 \
-    --save /media/elvircrn/6e3c126c-c6bb-43eb-9d82-1e59b2111688/ecrncevi/SpQR/output0
+    --save <output_path>
