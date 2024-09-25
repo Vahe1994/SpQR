b90e64d9845f46f9b65c2a395c3097964d70e643
Elvir Crncevic <elvircrn@gmail.com>
2024-09-25 17:39:16 +0200

    Optimize inference

--- inference/__init__.py
+++ inference/__init__.py
@@ -104,6 +104,8 @@ class SPQRModule(torch.nn.Module):
         self.deq_w = None
         self.in_perm = spqr_host.in_perm
         self.out_perm = spqr_host.out_perm
+        self.y = torch.zeros((1, 10, self.m), dtype=torch.float16, device=self.buff0.device)
+        self._y = torch.zeros(self.m, dtype=torch.float16, device=self.buff0.device)
 
     def to_device(self, device: torch.device):
         self.buff0 = self.buff0.to(device=device)
@@ -141,10 +143,8 @@ class SPQRModule(torch.nn.Module):
 
     def forward(self, x: T) -> T:
         inner_dim = x.shape[1]
-        y = torch.zeros((1, inner_dim, self.m), dtype=x.dtype, device=x.device)
 
         for i in range(inner_dim):
-            _y = torch.zeros(self.m, dtype=x.dtype, device=x.device)
             _x = x[0, i, :].flatten()
             if self.in_perm is not None:
                 _x = _x[self.in_perm]
@@ -160,18 +160,17 @@ class SPQRModule(torch.nn.Module):
                 self.nnz,
                 # TODO: Might case a CPU regression
                 _x,
-                _y,
-                FeatureFlag.SPARSE_FUSED_FP32_ASYNC)
+                self._y,
+                FeatureFlag.SPARSE_FUSED_FP32)
             if self.out_perm is not None:
                 out_perm_long = self.out_perm
-                _y = _y[out_perm_long]
-            y[0, i, :] = _y
-        torch.cuda.synchronize(x.device)
+                self._y = self._y[out_perm_long]
+            self.y[0, i, :] = self._y
 
         # end_time = time.time()
         # duration = end_time - start_time
         # print(f'\t\t{duration:.10f}')
-        return y
+        return self.y
 
 
 # Compression
