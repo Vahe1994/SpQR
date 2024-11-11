25d4f8025d81ca01172245e9126c51109f89054b
Elvir Crncevic <elvircrn@gmail.com>
2024-11-11 20:20:03 +0100

    Ramp down thread count

--- inference/spqr/spqr_cuda_kernel.cu
+++ inference/spqr/spqr_cuda_kernel.cu
@@ -543,9 +543,9 @@ int spqr_matvec(
 
   if (is_csr) {
     if (prob_m % 16 == 0 && prob_n % 1024 == 0) {
-      CALL_FUSED(spqr_quantized_matvec_fused_csr, 1, 64, 1, true);
+      CALL_FUSED(spqr_quantized_matvec_fused_csr, 1, 16, 1, true);
     } else if (prob_m % 16 == 0 && prob_n % 512 == 0) {
-      CALL_FUSED(spqr_quantized_matvec_fused_csr, 1, 32, 1, true);
+      CALL_FUSED(spqr_quantized_matvec_fused_csr, 1, 16, 1, true);
     } else if (prob_m % 16 == 0 && prob_n % 256 == 0) {
       CALL_FUSED(spqr_quantized_matvec_fused_csr, 1, 16, 1, true);
     } else if (prob_m % 16 == 0 && prob_n % 128 == 0) {
@@ -559,9 +559,9 @@ int spqr_matvec(
     }
   } else {
     if (prob_m % 16 == 0 && prob_n % 1024 == 0) {
-      CALL_FUSED(spqr_quantized_matvec_fused_csr, 1, 64, 1, false);
+      CALL_FUSED(spqr_quantized_matvec_fused_csr, 1, 16, 1, false);
     } else if (prob_m % 16 == 0 && prob_n % 512 == 0) {
-      CALL_FUSED(spqr_quantized_matvec_fused_csr, 1, 32, 1, false);
+      CALL_FUSED(spqr_quantized_matvec_fused_csr, 1, 16, 1, false);
     } else if (prob_m % 16 == 0 && prob_n % 256 == 0) {
       CALL_FUSED(spqr_quantized_matvec_fused_csr, 1, 16, 1, false);
     } else if (prob_m % 16 == 0 && prob_n % 128 == 0) {
