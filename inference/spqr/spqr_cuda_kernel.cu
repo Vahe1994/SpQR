9ed52179afb5ad7bbfda0f7c75d3c473661e51b3
Elvir Crncevic <elvircrn@gmail.com>
2024-11-11 20:18:20 +0100

    Try ramping up thread count

--- inference/spqr/spqr_cuda_kernel.cu
+++ inference/spqr/spqr_cuda_kernel.cu
@@ -542,7 +542,9 @@ int spqr_matvec(
 
 
   if (is_csr) {
-    if (prob_m % 16 == 0 && prob_n % 512 == 0) {
+    if (prob_m % 16 == 0 && prob_n % 1024 == 0) {
+      CALL_FUSED(spqr_quantized_matvec_fused_csr, 1, 64, 1, true);
+    } else if (prob_m % 16 == 0 && prob_n % 512 == 0) {
       CALL_FUSED(spqr_quantized_matvec_fused_csr, 1, 32, 1, true);
     } else if (prob_m % 16 == 0 && prob_n % 256 == 0) {
       CALL_FUSED(spqr_quantized_matvec_fused_csr, 1, 16, 1, true);
@@ -556,7 +558,9 @@ int spqr_matvec(
       CALL_FUSED(spqr_quantized_matvec_fused_csr, 1, 1, 1, true);
     }
   } else {
-    if (prob_m % 16 == 0 && prob_n % 512 == 0) {
+    if (prob_m % 16 == 0 && prob_n % 1024 == 0) {
+      CALL_FUSED(spqr_quantized_matvec_fused_csr, 1, 64, 1, false);
+    } else if (prob_m % 16 == 0 && prob_n % 512 == 0) {
       CALL_FUSED(spqr_quantized_matvec_fused_csr, 1, 32, 1, false);
     } else if (prob_m % 16 == 0 && prob_n % 256 == 0) {
       CALL_FUSED(spqr_quantized_matvec_fused_csr, 1, 16, 1, false);
