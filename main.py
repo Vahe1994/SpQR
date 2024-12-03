76ec14a717b36d955fa9bcd2e7f562fbda219259
Elvir Crncevic <elvircrn@gmail.com>
2024-12-03 14:20:56 +0100

    Transfer the model to device

--- main.py
+++ main.py
@@ -576,6 +576,7 @@ if __name__ == "__main__":
     print("============  Loading model... ============")
     model = get_model(args.model_path, args.load, args.dtype).train(False)
 
+    model = model.to(device=device)
     print("\n============ Quantizing model... ============")
     if args.wbits < 16 and args.load:
         print("\n Warning: You are quantizing quantized model!")
